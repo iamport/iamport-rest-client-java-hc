d37951faa3f46226093579da74e17ef88028874e
Ama <ama@siot.do>
2016-12-23 14:48:39 +0900

    jar 추가 및 README 내용수정

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # iamport-rest-client-java-hc
-Apache HttpClient기반의 java용 아임포트 REST API클라이언트입니다.
+JAVA 1.5, 1.6버전 사용자들을 위한 Apache HttpClient기반의 java용 아임포트 REST API클라이언트입니다.
+
+기존에 제공된 JAVA 1.7 이상 버전용 Client - [https://github.com/iamport/iamport-rest-client-java](https://github.com/iamport/iamport-rest-client-java)
+
+## 설치
+maven plugin을 다음과 같이 추가하시거나, build된 [jar](build/iamport-rest-client-java-hc-0.1.1-SNAPSHOT.jar) 파일을 다운로드하세요.
+
+```xml
+<dependency>
+	<groupId>com.siot.iamport</groupId>
+	<artifactId>iamport-rest-client-java-hc</artifactId>
+	<version>0.1.1-SNAPSHOT</version>
+</dependency>
+```
+
+## 구현된 API
+- POST /users/getToken
+- GET /payments/{imp_uid}
+- GET /payments/{imp_uid}
+- POST /payments/cancel
+
+## 예제
+src/test/java 의 IamportClientTest.java를 참조해주세요
--- src/test/java/com/siot/IamportRestHttpClientJava/IamportClientTest.java
+++ src/test/java/com/siot/IamportRestHttpClientJava/IamportClientTest.java
@@ -1,11 +1,54 @@
 package com.siot.IamportRestHttpClientJava;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import com.siot.IamportRestHttpClientJava.request.CancelData;
+import com.siot.IamportRestHttpClientJava.response.IamportResponse;
+import com.siot.IamportRestHttpClientJava.response.Payment;
+
 public class IamportClientTest {
 	IamportClient client;
 	
-	public void setup() {
-		String test_api_key = "imp_apikey";
-		String test_api_secret = "ekKoeW8RyKuT0zgaZsUtXXTLQ4AhPFW3ZGseDA6bkA5lamv9OqDMnxyeB9wqOsuO9W3Mx9YSJ4dTqJ3f";
-		client = new IamportClient(test_api_key, test_api_secret);
+	@Before
+	public void setup() throws Exception {
+		String api_key = "imp_apikey";
+		String api_secret = "ekKoeW8RyKuT0zgaZsUtXXTLQ4AhPFW3ZGseDA6bkA5lamv9OqDMnxyeB9wqOsuO9W3Mx9YSJ4dTqJ3f";
+
+		client = new IamportClient(api_key, api_secret);
+	}
+	
+	@Test
+	public void testGetToken() throws Exception {
+		String token = client.getToken();
+		System.out.println("token : " + token);
+	}
+	
+	@Test
+	public void testGetPaymentByImpUid() throws Exception {		
+		IamportResponse<Payment> paymentByimpuid = client.paymentByImpUid("imps_146154615717");
+		System.out.println(paymentByimpuid.getResponse().getImpUid());
+	}
+
+	@Test
+	public void testGetPaymentByMerchantUid() throws Exception {		
+		IamportResponse<Payment> paymentByMerchantUid = client.paymentByMerchantUid("merchant_1448006668555");
+		System.out.println(paymentByMerchantUid.getResponse().getMerchantUid());
+	}
+	
+	@Test
+	public void testCancelPaymentByImpUid() throws Exception {
+		//이미 취소된 거래 imp_uid
+		CancelData cancel1 = new CancelData("imp_1411648292162", true);
+		IamportResponse<Payment> cancelpayment1 = client.cancelPayment(cancel1);
+		System.out.println(cancelpayment1.getMessage());
+	}
+	
+	@Test
+	public void testCancelPaymentByMerchantUid() throws Exception {
+		//이미 취소된 거래 merchant_uid
+		CancelData cancel2 = new CancelData("iamport_1416218882181", false);
+		IamportResponse<Payment> cancelpayment2 = client.cancelPayment(cancel2);
+		System.out.println(cancelpayment2.getMessage());
 	}
 }
